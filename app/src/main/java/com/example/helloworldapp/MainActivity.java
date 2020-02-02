3bff7628cc166ea379f7c54d97d0ca61d5312a70
Frederic Triplett, Jr <frederic1.triplett@famu.edu>
2020-02-02 04:10:07 -0500

    Finished required user stories for pre-work app.
    ***NOTE***
    Finish stretch stories then resubmit.

--- app/src/main/java/com/example/helloworldapp/MainActivity.java
+++ app/src/main/java/com/example/helloworldapp/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.helloworldapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvGreeting;
+    Button btnTextColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnTxtColor).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        ((TextView) findViewById(R.id.tvGreeting)).setTextColor(getResources().getColor(R.color.colorChange));
+                    }
+                }
+        );
     }
 }
